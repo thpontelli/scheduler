5450053934cae3d1ec6b31cfbf016f73836f783e
Thiago Pontelli <thiago.pontelli@gmail.com>
2023-06-11 21:32:48 -0400

    Add Schedule

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
-import InterviewerList from "./InterviewerList";
-import InterviewerListItem from "./InterviewerListItem";
-
+import Appointment from "./Appointment";
 
 const days = [
   {
@@ -23,19 +21,69 @@ const days = [
   },
 ];
 
-const interviewers = [
-  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
-];
+// const interviewers = [
+//   { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+//   { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+//   { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+//   { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+//   { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+// ];
+
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  },
+  "last": {
+    id: 6,
+    time: "5pm"
+  }
+};
+
+const scheduleList = Object.values(appointments).map((schedule) => {
+  return (
+    <Appointment
+      key={schedule.id}
+      {...schedule}
+    />
+  )
+})
 
 export default function Application(props) {
 
   const [day, setDay] = useState("Monday");
-
-  const [interviewer, setInterviewer] = useState(3);
 
   return (
     <main className="layout">
@@ -59,12 +107,9 @@ export default function Application(props) {
           alt="Lighthouse Labs"
         />      </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-        <InterviewerList
-          interviewers={interviewers}
-          interviewer={interviewer}
-          setInterviewer={setInterviewer}
-        />
+        <ul>
+          {scheduleList}
+        </ul>
       </section>
     </main>
   );
