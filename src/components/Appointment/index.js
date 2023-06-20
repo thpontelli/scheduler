8c8dd596411bbd5792dd9893c9de982cd592b475
Thiago Pontelli <thiago.pontelli@gmail.com>
2023-06-19 21:07:25 -0400

    Add modes

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,20 +1,76 @@
-import React from "react";
+import React, { useEffect } from "react";
+
 import "components/Appointment/styles.scss";
+
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+import Confirm from "./Confirm";
+import Error from "./Error";
+import Status from "./Status";
+
+import useVisualMode from "hooks/useVisualMode";
 
 
 export default function Appointment(props) {
+
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const SAVING = "SAVING";
+  const ERROR_SAVE = "ERROR_SAVE";
+
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+  useEffect(() => {
+    
+    if (props.interview && mode === EMPTY) {
+      transition(SHOW);
+    }
+    
+    if (!props.interview && mode === SHOW) {
+      transition(EMPTY);
+    }
+
+  }, [mode, transition, props.interview])
+
+  function save(name, interviewer) {
+    
+    if (name && interviewer) {
+      transition(SAVING);
+
+      const interview = {
+        student: name,
+        interviewer
+      };
+
+      props.bookInterview(props.id, interview)
+        .then(() => transition(SHOW))
+        .catch(() => transition(ERROR_SAVE, true))
+    }
+  }
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview
-        ? <Show
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
-        : <Empty
+      )}
+      {mode === CREATE &&
+        <Form
+          name={props.name}
+          value={props.value}
+          interviewers={[]}
+          onSave={save}
+          onCancel={back}
         />
       }
     </article>
