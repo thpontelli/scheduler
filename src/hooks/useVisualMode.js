ac292974ae3aae3e2ae428b70bf7c122c1696f31
Thiago Pontelli <thiago.pontelli@gmail.com>
2023-06-23 20:09:14 -0400

    Refactor code

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,7 +1,7 @@
 import {useState} from "react";
 
 export default function useVisualMode(initial){
-  //I used let instead of const
+ 
   let [mode, setMode] = useState(initial);
   let [history, setHistory] = useState([initial]); 
 
@@ -28,31 +28,7 @@ export default function useVisualMode(initial){
   return { mode, transition, back};
 }
 
-// import { useState } from "react";
 
-// export default function useVisualMode(initialMode) {
-//   const [mode, setMode] = useState(initialMode);
-//   const [history, setHistory] = useState([initialMode]); // This line is new!
-
-//   const transition = function (newMode, replace = false) {
-//     if (replace === true) {
-//       let last = history.pop();
-//     }
-
-//     history.push(newMode);
-//     setMode(newMode);
-//   }
-
-//   const back = function () {
-//     if (history.length > 1) {
-//       setMode(history[history.length - 2]);
-//       setHistory(history.slice(0, history.length - 1));
-//     }
-//   }
-
-
-//   return { mode, transition, back }
-// };
 
 
 
