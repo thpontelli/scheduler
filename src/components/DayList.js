12c526cc1b7b610bf08aba884b1990d3c2fd4c3f
Thiago Pontelli <thiago.pontelli@gmail.com>
2023-07-02 16:22:13 -0400

    Update DayList.js

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,16 +3,15 @@ import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
 
-  const listDays = props.days.map((day) =>
-    <li><DayListItem
+  const listDays = props.days.map((day) => {
+    return <DayListItem
       key={day.id}
       name={day.name}
       spots={day.spots}
       selected={day.name === props.day}
       setDay={props.setDay}
     />
-    </li>
-  );
+  });
 
   return (
     <ul>
